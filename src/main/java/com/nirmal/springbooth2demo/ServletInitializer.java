ce73420a0b1f2c02894c96c7d2bcc2c95bd419d9
Nirmal Sharma <nirmal.sharma3500@gmail.com>
2023-12-25 13:31:59 +0530

    Initial Commit

---
+++ src/main/java/com/nirmal/springbooth2demo/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.nirmal.springbooth2demo;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootH2DemoApplication.class);
+	}
+
+}
---
+++ src/test/java/com/nirmal/springbooth2demo/SpringbootH2DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirmal.springbooth2demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootH2DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
